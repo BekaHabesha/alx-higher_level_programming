53608f20afaf5219c1f517e9c2d50ec69b14afd2
bekahabesha <139154433+BekaHabesha@users.noreply.github.com>
2024-03-01 15:44:59 -0800

    1-hbtn_header.py - No1.Response header value #0.

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 # Author - Bereket Dereje
-"""
-A Python script that:
+"""A Python script that:
   - takes in a URL,
   - sends a request to the URL 
   - and displays the value of the X-Request-Id variable
