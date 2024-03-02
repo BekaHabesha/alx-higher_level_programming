19c557d57e2e177ec70b61b1fbfd0610953919c7
bekahabesha <139154433+BekaHabesha@users.noreply.github.com>
2024-03-01 16:43:09 -0800

    5-hbtn_header.py - No5.Response header value #1.

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-# Author - Bereket Dereje
-"""
-Python script that:
-  - takes in a URL, sends a request to the URL and 
-  - displays the value of the variable X-Request-Id in the response header
+"""Displays the X-Request-Id header variable of a request to a given URL
 """
 import sys
 import requests
